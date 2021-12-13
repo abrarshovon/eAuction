2402f5ad46f787b6e4ef479bcb18cb611c57b8d8
abrarshovon <abrarfaisal306@gmail.com>
2021-12-13 17:06:00 +0600

    First Step

---
+++ eAuction/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from .forms import CustomUserCreationForm
+from django.views.generic import TemplateView,CreateView
+
+class MainHome(TemplateView):
+    template_name = "eAuction/main.html"
+
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
